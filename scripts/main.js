ed214ad10f35362ace3f8bf5ac9728afdc7d1a1c
kopever <kop.still@gmail.com>
2020-05-12 00:25:25 +0800

    Getting_started_with_the_web/JavaScript_basics

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,32 @@
+let myImage = document.querySelector("img");
+myImage.onclick = function () {
+	let mySrc = myImage.getAttribute("src");
+	if (mySrc === "images/firefox-icon.png") {
+		myImage.setAttribute("src", "images/firefox2.png");
+	} else {
+		myImage.setAttribute("src", "images/firefox-icon.png");
+	}
+};
+
+let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello, world!";
+
+function setUserName() {
+	let myName = prompt("请输入你的名字。");
+	if (!myName || myName === null) {
+		setUserName();
+	}
+	localStorage.setItem("name", myName);
+	myHeading.textContent = "Mozilla酷毙了，" + myName;
+}
+
+if (!localStorage.getItem("name")) {
+	setUserName();
+} else {
+	let storedName = localStorage.getItem("name");
+	myHeading.textContent = "Mozilla酷毙了，" + storedName;
+}
+
+myButton.onclick = function () {
+	setUserName();
+};
